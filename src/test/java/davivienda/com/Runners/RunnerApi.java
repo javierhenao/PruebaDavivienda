d1278aa0500788a78ed4df2b006a8521b2dcc0ec
Javier Leonardo Henao <leonardo.henaoz@hotmail.com>
2025-11-01 23:11:57 -0500

    ✅ Adicionar API

--- src/test/java/davivienda/com/Runners/RunnerApi.java
+++ src/test/java/davivienda/com/Runners/RunnerApi.java
@@ -4,20 +4,8 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 
-
-
 @RunWith(net.serenitybdd.cucumber.CucumberWithSerenity.class)
-@CucumberOptions(
-    features = "src/test/resources/feature",
-    glue = {"davivienda.com.definitions"},
-    tags = "@PruebaAPI",                 // filtra por tag si quieres
-    monochrome = true,
-    plugin = {"pretty"}
-)
-public class RunnerApi { }
-
-
- 
-
-
-
+@CucumberOptions(features = "src/test/resources/feature", glue = { "davivienda.com.definitions" }, tags = "@PruebaAPI",
+		monochrome = true, plugin = { "pretty" })
+public class RunnerApi {
+}
--- src/test/java/davivienda/com/definitions/EmpleadosApiDefinition.java
+++ src/test/java/davivienda/com/definitions/EmpleadosApiDefinition.java
@@ -7,34 +7,32 @@ import davivienda.com.Api.OrangeHrmApi;
 
 public class EmpleadosApiDefinition {
 
-    OrangeHrmApi api = new OrangeHrmApi(System.getProperty(
-        "orange.api.base",
-        "https://opensource-demo.orangehrmlive.com"
-    ));
-    Response resp;
+	OrangeHrmApi api = new OrangeHrmApi(
+			System.getProperty("orange.api.base", "https://opensource-demo.orangehrmlive.com"));
+	Response resp;
 
-    @Given("^inicio sesión en la API con usuario \"([^\"]+)\" y clave \"([^\"]+)\"$")
-    public void loginApi(String user, String pass) {
-        api.login(user, pass); // prepara cookies de sesión
-    }
+	@Given("^inicio sesión en la API con usuario \"([^\"]+)\" y clave \"([^\"]+)\"$")
+	public void loginApi(String user, String pass) {
+		api.login(user, pass); // prepara cookies de sesión
+	}
 
-    @When("^consulto la API de empleados por nombre \"([^\"]+)\"$")
-    public void consultarApiPorNombre(String nombreCompleto) {
-        resp = api.buscarEmpleadosPorNombre(nombreCompleto);
-    }
+	// PRUEBA
 
-    @Then("^la respuesta debe tener status (\\d+)$")
-    public void validarStatus(int esperado) {
-        assertEquals("Código de respuesta inesperado", esperado, resp.statusCode());
-    }
+	@When("^consulto la API de empleados por nombre \"([^\"]+)\"$")
+	public void consultarApiPorNombre(String nombreCompleto) {
+		resp = api.buscarEmpleadosPorNombre(nombreCompleto);
+	}
 
-    @And("^la respuesta debe incluir el nombre \"([^\"]+)\" y el apellido \"([^\"]+)\"$")
-    public void validarNombreApellido(String nombre, String apellido) {
-        String body = resp.asString();
-        assertTrue("No se encontró el nombre en la respuesta", body.contains(nombre));
-        assertTrue("No se encontró el apellido en la respuesta", body.contains(apellido));
-    }
-    
-    
+	@Then("^la respuesta debe tener status (\\d+)$")
+	public void validarStatus(int esperado) {
+		assertEquals("Código de respuesta inesperado", esperado, resp.statusCode());
+	}
+
+	@And("^la respuesta debe incluir el nombre \"([^\"]+)\" y el apellido \"([^\"]+)\"$")
+	public void validarNombreApellido(String nombre, String apellido) {
+		String body = resp.asString();
+		assertTrue("No se encontró el nombre en la respuesta", body.contains(nombre));
+		assertTrue("No se encontró el apellido en la respuesta", body.contains(apellido));
+	}
 
 }
